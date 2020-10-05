576b9214e292e852646f8385678e04b45a045c13
JeanPierreLeDouche <38559253+JeanPierreLeDouche@users.noreply.github.com>
2020-10-05 10:06:25 +0200

    Update CalculateAngles.py
    test2

--- CalculateAngles.py
+++ CalculateAngles.py
@@ -5,9 +5,8 @@ Created on Mon Oct  5 09:42:15 2020
 @author: Gebruiker
 """
 
-<<<<<<< Updated upstream
-=======
+
 import numpy as np
 
 print("Hello world, groetjes van Ivo")
->>>>>>> Stashed changes
+print('waar komt deze verandering terecht?')
