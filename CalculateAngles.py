47bad9db1776038d0848fdf3a1f7d9e832f42f94
RubenScheffer <60524485+Rubenscheffer@users.noreply.github.com>
2020-10-06 15:07:43 +0200

    Update CalculateAngles.py
    Added plot for a singlue bouy as test, also added the new borders of our area of interest

--- CalculateAngles.py
+++ CalculateAngles.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  5 09:42:15 2020
-
-@author: Gebruiker
-"""
 
 
+import pandas as pd
 import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
-print("Hello world, groetjes van Ivo")
-print('waar komt deze verandering terecht?')
+data = pickle.load(open("BuoyDatabase.p", "rb"))
+
+# print(data.head())
+
+#Test to plot a single buoy path
+
+west_border = 310
+east_border = 350
+north_border = 60
+south_border = 45
+
+
+buoy_1 = data.loc[data['ID'] == 72615]
+
+
+fig = plt.figure()
+
+ax = fig.add_subplot(1,1,1, projection=ccrs.PlateCarree())
+ax.scatter(buoy_1['Lon'],buoy_1['Lat'], s=2)
+ax.coastlines()
+ax.gridlines(draw_labels=True, dms=True)
+ax.set_extent([west_border, east_border, north_border, south_border])
+ax.add_feature(cfeature.OCEAN)
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.BORDERS)
+ax.add_feature(cfeature.COASTLINE)
+
+
+plt.show()
+
+
