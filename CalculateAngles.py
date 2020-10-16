4f31015258cab90c769ad04a113a1060f0e002c0
JeanPierreLeDouche <38559253+JeanPierreLeDouche@users.noreply.github.com>
2020-10-16 12:28:36 +0200

    correctly calculates angles from -180,180 now

--- CalculateAngles.py
+++ CalculateAngles.py
@@ -13,6 +13,8 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from time import perf_counter
 
+r_e = 6371 * 1e3 #m 
+
 data = pickle.load(open("BuoyDatabase.p", "rb"))
 
 #Test to plot a single buoy path
@@ -22,6 +24,23 @@ east_border = 350
 north_border = 60
 south_border = 45
 
+def lat_corr_distance(lon_distance, lat):
+    corrected = 2 * np.pi * r_e * np.cos(lat * 180 / np.pi ) /360 * lon_distance
+    return corrected # in m 
+
+def angle_between_vecs(vector1, vector2):
+    x1 = vector1[0]
+    y1 = vector1[1]
+    
+    x2 = vector2[0]
+    y2 = vector2[1]    
+        
+    dot = x1*x2 + y1*y2      # dot product between [x1, y1] and [x2, y2]
+    det = x1*y2 - y1*x2      # determinant
+    angle = np.arctan2(det, dot) * 180 / np.pi  # angle in degrees [-180, 180]
+    
+    return angle
+
 buoy_IDs = np.unique(data["ID"]) # as of current there are about 10 000
 dates = np.unique(data["Date"]) # about 16 000
 
@@ -30,46 +49,40 @@ M_angles = pd.DataFrame()
 angles_col_np = np.zeros((16000))
 
 # loop through each buoy and then go by time (second for loop)
-t_start = perf_counter()
-
-for ID in buoy_IDs[:0]:
+for ID in buoy_IDs[:10]:
     current_buoy_data = data.loc[data['ID'] == ID]
     buoy_lats_lons = current_buoy_data[['Lat','Lon']]
      
     x_previous = pd.DataFrame([[0,0]])
-    x_current = pd.DataFrame([[0],[0]])
-            
+    x_current = pd.DataFrame([[0,0]])
+    x_next = pd.DataFrame([[0,0]])
+    
+
     
     for time in np.arange(buoy_lats_lons.shape[0]-2):
         
         # use three spatial positions at a time to calculate the vectors from
         # first to second and from second to third, giving two consecutive 
         # vectors 
-        t_0 = perf_counter()
-        x_previous = buoy_lats_lons.iloc[time,:]
-        x_current = buoy_lats_lons.iloc[time+1,:]
+        
+        x_prev = buoy_lats_lons.iloc[time,:]
+        x_curr = buoy_lats_lons.iloc[time+1,:]
         x_next = buoy_lats_lons.iloc[time+2,:]
         
+        dx_1 = lat_corr_distance((x_prev[1] - x_curr[1]), x_curr[0])
+        dx_2 = lat_corr_distance((x_next[1] - x_curr[1]), x_curr[0])
         
-        vec1 = x_current-x_previous
-        vec2 = x_next - x_current
+        dy_1 =( x_prev[0] - x_curr[0])* 2* np.pi * r_e / 360
+        dy_2 =( x_next[0] - x_curr[0]) * 2* np.pi * r_e / 360 
         
+        vec1 = pd.DataFrame([[dx_1, dy_1]])
+        vec2 = pd.DataFrame([[dx_2, dy_2]])        
         
-        # using that the dot product of a,b is defined : a.b = |a||b|cos(angle)
-        # where the angle is the angle between the vectors in this case, we can
-        # exploit this definition by using that angle = arccos(a.b / |a||b|)
-        
-        dot = vec1.dot(vec2.transpose())
-        
-        normalized_dot = dot/ (np.linalg.norm(vec1)*np.linalg.norm(vec2))
-        
-        angle = np.arccos(normalized_dot) * 180 /np.pi 
-        #  
+        angle = angle_between_vecs(vec1, vec2)
         
         # angles are stored in a numpy array         
         angles_col_np[time] = angle
-        
-
+    
     # after looping through all timesteps for one buoy the numpy array is copied
     # as a pandas series which can then be appended into a pandas DataFrame. 
     # the resulting dataframe has buoy IDs as headers and angles per belonging
@@ -79,8 +92,6 @@ for ID in buoy_IDs[:0]:
     
     angles_col_pd = pd.Series(angles_col_np)
     M_angles[str(ID)] = angles_col_pd 
-
-
 
 # after masking all the trailing zeros we get lists of timestep angles per buoy    
 M_angles = M_angles.mask(M_angles == 0)    
