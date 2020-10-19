7d50edf6be6af9a673825f91a81cf7028df58f40
JeanPierreLeDouche <38559253+JeanPierreLeDouche@users.noreply.github.com>
2020-10-19 09:16:28 +0200

    Trying to find some way to deal with variable array sizes

--- CalculateAngles.py
+++ CalculateAngles.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from time import perf_counter
+import numpy.ma as ma
 
 r_e = 6371 * 1e3 #m 
 
@@ -45,21 +46,24 @@ buoy_IDs = np.unique(data["ID"]) # as of current there are about 10 000
 dates = np.unique(data["Date"]) # about 16 000
 
 # initialize list where all the angles will go and one temp array
-M_angles = pd.DataFrame()
-angles_col_np = np.zeros((16000))
+# M_angles = pd.DataFrame()
+# M_angles = np.ndarray(buoy_IDs.shape[0], dtype=object)
+M_angles = np.zeros((10000, 16000 ))
 
 t1 = perf_counter()
+ID_counter = 0
 
 # loop through each buoy and then go by time (second for loop)
-for ID in buoy_IDs[:10]:
+for ID in buoy_IDs[:100]:
     current_buoy_data = data.loc[data['ID'] == ID]
     buoy_lats_lons = current_buoy_data[['Lat','Lon']]
     
     x_previous = np.array([0,0])
     x_current = np.array([0,0])
     x_next = np.array([0,0])
-
     
+    angles_col = np.zeros((16000))
+
     for time in np.arange(buoy_lats_lons.shape[0]-2):
         
         # use three spatial positions at a time to calculate the vectors from
@@ -82,63 +86,60 @@ for ID in buoy_IDs[:10]:
         angle = angle_between_vecs(vec1, vec2)
         
         # angles are stored in a numpy array         
-        angles_col_np[time] = angle
+        angles_col[time] = angle
         
-   
+    angles_col = np.trim_zeros(angles_col)
+    
     # after looping through all timesteps for one buoy the numpy array is copied
     # as a pandas series which can then be appended into a pandas DataFrame. 
     # the resulting dataframe has buoy IDs as headers and angles per belonging
     # to this buoy per timestep in each column
     
+    M_angles[ID_counter,:angles_col.shape[0]] = angles_col
+    ID_counter = ID_counter + 1
 
-        
-    angles_col_pd = pd.Series(angles_col_np)
-    angles_col_pd = angles_col_pd.mask(angles_col_pd == 0)
-    angles_col_pd = angles_col_pd.dropna(axis = 0)
-    
-    M_angles[str(ID)] = angles_col_pd 
-    
 t2 = perf_counter()
 print("calculation took: ", (t2-t1), " seconds")
 
+t3 = perf_counter()
+
+zeros_count = 0
+for i in range(M_angles.shape[0]):
+    for j in range(M_angles.shape[1]):
+        if M_angles[i,j] == 0:
+            M_angles[i,j] = None
+            zeros_count = zeros_count + 1
+            
+print("counted: ", zeros_count, "zeros")
+
+t4= perf_counter() 
+print("dealing with the zeros took:", (t4 - t3), " seconds")
 
 # after masking all the trailing zeros we get lists of timestep angles per buoy    
 # M_angles = M_angles.mask(M_angles == 0)    
-M_angles.to_csv()
+# M_angles.to_csv()
 
 ###--------------------------------------------------------------------------
 #%%
 
-buoy_1 = data.loc[data['ID'] == 34471]
+# buoy_1 = data.loc[data['ID'] == 34471]
 
-## plot of 1 buoy that Ruben made
-fig = plt.figure()
+# ## plot of 1 buoy that Ruben made
+# fig = plt.figure()
 
-ax = fig.add_subplot(1,1,1, projection=ccrs.PlateCarree())
-ax.scatter(buoy_1['Lon'],buoy_1['Lat'], s=2)
-ax.coastlines()
-ax.gridlines(draw_labels=True, dms=True)
-ax.set_extent([west_border, east_border, north_border, south_border])
-ax.add_feature(cfeature.OCEAN)
-ax.add_feature(cfeature.LAND)
-ax.add_feature(cfeature.BORDERS)
-ax.add_feature(cfeature.COASTLINE)
-plt.show()
-
-# buoy_1_angles = M_angles
-# # M_his = M_angles
-# # M_his = M_his.dropna(axis = 0)
-# # fig2 = plt.figure()
-# # plt.hist(M_his, 360)
-# # plt.show()
-
-# fig2 = plt.figure()
-# plt.hist(M_angles['34471'], 360)
+# ax = fig.add_subplot(1,1,1, projection=ccrs.PlateCarree())
+# ax.scatter(buoy_1['Lon'],buoy_1['Lat'], s=2)
+# ax.coastlines()
+# ax.gridlines(draw_labels=True, dms=True)
+# ax.set_extent([west_border, east_border, north_border, south_border])
+# ax.add_feature(cfeature.OCEAN)
+# ax.add_feature(cfeature.LAND)
+# ax.add_feature(cfeature.BORDERS)
+# ax.add_feature(cfeature.COASTLINE)
 # plt.show()
 
-# TO DO: zijn hoeken normaal verdeeld ? 
-# paper lezen:
-    # decorrelatie tijdsschaal 
-# hoe kun je deze hoeken gebruiken om iets over diffusie te zeggen ? 
+#%%
+
+plt.hist(M_angles, 360)
 
 
